b5a7d9d0367841bdd978f9b36c96e366adeb8b67
islem.h <147746646+islem-haddada@users.noreply.github.com>
2024-06-12 05:25:18 +0100

    Add files via upload

---
+++ panel_page.php
@@ -0,0 +1,206 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$seller_id = $_SESSION['user_id'];
+
+if(!isset($seller_id)){
+   header('location:login.php');
+};
+
+if(isset($_POST['add_product'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $price = $_POST['price'];
+   $image = $_FILES['image']['name'];
+   $image_size = $_FILES['image']['size'];
+   $image_tmp_name = $_FILES['image']['tmp_name'];
+   $image_folder = 'uploaded_img/'.$image;
+
+   $select_product_name = mysqli_query($conn, "SELECT name FROM `products` WHERE name = '$name' AND seller_id = '$seller_id'") or die('query failed');
+   if(mysqli_num_rows($select_product_name) > 0){
+      $message[] = 'product name already added';
+   }else{
+      $add_product_query = mysqli_query($conn, "INSERT INTO `products`(name, price, image, seller_id) VALUES('$name', '$price', '$image', '$seller_id')") or die('query failed');
+
+      if($add_product_query){
+         if($image_size > 2000000){
+            $message[] = 'image size is too large';
+         }else{
+            move_uploaded_file($image_tmp_name, $image_folder);
+            $message[] = 'product added successfully!';
+         }
+      }else{
+         $message[] = 'product could not be added!';
+      }
+   }
+}
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_image_query = mysqli_query($conn, "SELECT image FROM `products` WHERE id = '$delete_id'") or die('query failed');
+   $fetch_delete_image = mysqli_fetch_assoc($delete_image_query);
+   unlink('uploaded_img/'.$fetch_delete_image['image']);
+   mysqli_query($conn, "DELETE FROM `products` WHERE id = '$delete_id'") or die('query failed');
+   header('location:panel_page.php');
+}
+
+if(isset($_POST['update_product'])){
+
+   $update_p_id = $_POST['update_p_id'];
+   $update_name = $_POST['update_name'];
+   $update_price = $_POST['update_price'];
+
+   mysqli_query($conn, "UPDATE `products` SET name = '$update_name', price = '$update_price' WHERE id = '$update_p_id'") or die('query failed');
+
+   $update_image = $_FILES['update_image']['name'];
+   $update_image_tmp_name = $_FILES['update_image']['tmp_name'];
+   $update_image_size = $_FILES['update_image']['size'];
+   $update_folder = 'uploaded_img/'.$update_image;
+   $update_old_image = $_POST['update_old_image'];
+
+   if(!empty($update_image)){
+      if($update_image_size > 2000000){
+         $message[] = 'image file size is too large';
+      }else{
+         mysqli_query($conn, "UPDATE `products` SET image = '$update_image' WHERE id = '$update_p_id'") or die('query failed');
+         move_uploaded_file($update_image_tmp_name, $update_folder);
+         unlink('uploaded_img/'.$update_old_image);
+      }
+   }
+
+   header('location:panel_page.php');
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Seller panel (Dashboard)</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom seller css file link  -->
+   <link rel="stylesheet" href="css/seller_style.css">
+
+</head>
+<body>
+   
+<?php include 'seller_header.php'; ?>
+
+<!-- seller dashboard section starts  -->
+
+<section class="dashboard">
+
+   <h1 class="title">dashboard</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <?php 
+            $select_products = mysqli_query($conn, "SELECT * FROM `products` WHERE seller_id = '$seller_id'") or die('query failed');
+            $number_of_products = mysqli_num_rows($select_products);
+         ?>
+         <h3><?php echo $number_of_products; ?></h3>
+         <p>products added</p>
+      </div>
+
+   </div>
+   
+</section>
+
+<!-- product CRUD section starts  -->
+
+<section class="add-products">
+
+   <h1 class="title">Add product</h1>
+
+   <form action="" method="post" enctype="multipart/form-data" style="font-size: 20px;">
+      Product Name:<input type="text" name="name" class="box" placeholder="enter product name" required><br>
+      Price: <input type="number" min="0" name="price" class="box" placeholder="enter product price" required>
+      <input type="file" name="image" accept="image/jpg, image/jpeg, image/png" class="box" >
+      <input type="submit" value="add product" name="add_product" class="btn">
+   </form>
+
+</section>
+
+<!-- product CRUD section ends -->
+
+<!-- show products  -->
+
+<section class="show-products">
+<h1 class="title">Add product</h1>
+   <div class="box-container">
+      <?php
+         $select_products = mysqli_query($conn, "SELECT * FROM `products` WHERE seller_id = '$seller_id'") or die('query failed');
+         if(mysqli_num_rows($select_products) > 0){
+            while($fetch_products = mysqli_fetch_assoc($select_products)){
+      ?>
+      <div class="box">
+         <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
+         <div class="name"><?php echo $fetch_products['name']; ?></div>
+         <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+         <a href="panel_page.php?update=<?php echo $fetch_products['id']; ?>" class="option-btn">update</a>
+         <a href="panel_page.php?delete=<?php echo $fetch_products['id']; ?>" class="delete-btn" onclick="return confirm('delete this product?');">delete</a>
+      </div>
+      <?php
+         }
+      }else{
+         echo '<p class="empty">no products added yet!</p>';
+      }
+      ?>
+   </div>
+
+</section>
+
+<section class="edit-product-form">
+
+   <?php
+      if(isset($_GET['update'])){
+         $update_id = $_GET['update'];
+         $update_query = mysqli_query($conn, "SELECT * FROM `products` WHERE id = '$update_id'") or die('query failed');
+         if(mysqli_num_rows($update_query) > 0){
+            while($fetch_update = mysqli_fetch_assoc($update_query)){
+   ?>
+   <form action="" method="post" enctype="multipart/form-data">
+      <input type="hidden" name="update_p_id" value="<?php echo $fetch_update['id']; ?>">
+      <input type="hidden" name="update_old_image" value="<?php echo $fetch_update['image']; ?>">
+      <img src="uploaded_img/<?php echo $fetch_update['image']; ?>" alt="">
+      <input type="text" name="update_name" value="<?php echo $fetch_update['name']; ?>" class="box" required placeholder="enter product name">
+      <input type="number" name="update_price" value="<?php echo $fetch_update['price']; ?>" min="0" class="box" required placeholder="enter product price">
+      <input type="file" class="box" name="update_image" accept="image/jpg, image/jpeg, image/png">
+      <input type="submit" value="update" name="update_product" class="btn">
+   </form>
+   <?php
+         }
+      }
+      }else{
+         echo '<script>document.querySelector(".edit-product-form").style.display = "none";</script>';
+      }
+   ?>
+
+</section>
+
+<!-- seller dashboard section ends -->
+
+
+
+
+
+
+
+
+
+<!-- custom seller js file link  -->
+<script src="js/seller_script.js"></script>
+
+</body>
+</html>
