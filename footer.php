939b68bbd1b586d0f261ad43869b6757c3ed7442
islem.h <147746646+islem-haddada@users.noreply.github.com>
2024-06-14 03:16:30 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,25 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @<?php echo date('Y'); ?> by <span>ISLAM MN</span> </p>
+
+</section>
